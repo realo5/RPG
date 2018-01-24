fafcd692fdec23bb9b3a916ae5922e60e8a86988
Angel Ellione <nightbreeze714@hotmail.com>
2018-01-24 18:06:07 -0500

    Added initial main method function calls.

--- RPG/Program.cs
+++ RPG/Program.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//We are drawing access to the SessionManager class by using it's encapsulating namespace. These are identical to the directory
+//hierarchy that has been designed to better seperate elements that are not of interest to one another immediately. Instead, we
+//will work to design channels of access to items as we need them by these using statements
+using SimpleRPGExample.Engine.Entities;
+//Check out the file hierarchy to see what I mean.
 
 namespace SimpleRPGExample
 {
-    class Game
+    static class Game
     {
-        void Main()
+        private static SessionManager _sessionManager;
+
+        static void Main()
         {
+            InitializeSessionManager();
 
             Console.ReadLine();
+        }
+
+        static void InitializeSessionManager()
+        {
+            //We will initialize objects in this fashion to better contain specific Entities in seperate manager classes.
+            _sessionManager = new SessionManager();
         }
     }
 }
