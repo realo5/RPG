e2245dc5b39b6e13412c7b0071b4b31cadacfccd
im.still.awake404 <im.still.awake404@gmail.com>
2018-01-30 18:41:30 -0500

    Added food and apple pie stuff -Jimson

---
+++ RPG/Engine/Entities/Items/Food.cs
@@ -0,0 +1,31 @@
+﻿using RPG.Engine.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPG.Engine.Entities.Items
+{
+    public abstract class Food : INameable
+    {
+        public string Presentation
+        {
+            get; set;
+        }
+        public string Name { get; set; }
+    }
+    public class Pie : Food, INameable
+    {
+        public string Filling
+        {
+            get; set;
+        }
+        public string Crust
+        {
+            get; set;
+        }
+
+
+    }
+}
